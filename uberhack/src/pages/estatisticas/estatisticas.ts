2c059ebff24291fbd8bf6fb8b9c8917cebef6648
Lucas França <lucas.franca@devolus.com>
2018-04-08 12:52:24 -0300

    resurcea

--- uberhack/src/pages/estatisticas/estatisticas.ts
+++ uberhack/src/pages/estatisticas/estatisticas.ts
@@ -1,9 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import * as Chart from 'chart.js';
 
 @Component({
     selector: 'page-estatisticas',
     templateUrl: 'estatisticas.html'
 })
 export class EstatisticasPage {
+    @ViewChild('myChart') private _ctx: ElementRef;
+
     constructor() {}
+
+    ionViewDidLoad() {
+        const data = {
+            datasets: [{
+                data: [10, 20, 30]
+            }],
+        
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Red',
+                'Yellow',
+                'Blue'
+            ]
+        };
+
+        const chart = new Chart(this._ctx.nativeElement, {
+            data,
+            type: 'polarArea',
+            options: {}
+        })
+    }
 }
